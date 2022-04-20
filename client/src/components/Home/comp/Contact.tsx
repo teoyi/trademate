b182565d93d11413900358a36cd21d620a77f54e
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-04-20 15:08:59 +0800

    completed contact section

--- client/src/components/Home/comp/Contact.tsx
+++ client/src/components/Home/comp/Contact.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaGithubAlt } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="h-screen flex flex-col justify-start w-full items-center xl:p-10">
+      <h1 className='w-full font-bold text-[36px] mb-2'>
+        <span className='text-primary-text'>Want to know more</span>
+        <span className='text-accent'>?</span>
+      </h1>
+      <p className="text-secondary-text xl:text-[15px] 2xl:text-[17px]">
+        Trademate is a work in progress by Luke Teo. Though it is his dream to create more open source projects that benefits someone down the road, it is still difficult for a single man to complete a full-scale application on his own. If you are interested in contibuting to this project, do not hesitate to contact him through the following methods: 
+      </p>
+      <div className='flex flex-row justify-center items-center h-full'> 
+        <a href="https://github.com/teoyi/" target="_blank" className='flex flex-col justify-center items-center hover:scale-105'>
+          <i className='text-accent xl:text-[150px] 2xl:text-[200px]'><FaGithubAlt /></i>
+          <p className='2xl:text-[24px]'>@teoyi</p>
+        </a>
+        <p className='mx-20 2xl:mx-[10rem] 2xl:text-[25px]'>or</p>
+        <a href="mailto:yipengteo.luke@gmail.com" className='flex flex-col justify-center items-center hover:scale-105'>
+          <i className="text-accent xl:text-[150px] 2xl:text-[200px]"><SiGmail /></i>
+          <p className='2xl:text-[24px]'>yipengteo.luke@gmail.com</p>
+        </a>
+      </div>
+    </section>
   )
 }
 
