68594261ca448e3d7bcec589670071e3a5dc5645
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-05-22 22:04:21 +0800

    added z index to hamburger menu. Introduced margin and font size to mobile menu

--- client/src/components/Nav/MobileNavbar.tsx
+++ client/src/components/Nav/MobileNavbar.tsx
@@ -6,17 +6,19 @@ const MobileNavbar = () => {
     <div className="md:hidden h-[100px] flex px-7 sm:px-10">
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className=" font-bold text-2xl md:text-sm">
-              <Link to="/">
-                  <span>trademate</span>
-                  <span className="text-accent">.</span>
-              </Link>
-          </h1>
-        <Hamburger direction="right" easing="ease-in" rounded />
+            <Link to="/">
+                <span>trademate</span>
+                <span className="text-accent">.</span>
+            </Link>
+        </h1>
+        <div className="z-20">
+          <Hamburger direction="right" easing="ease-in" rounded />
+        </div>
       </div>
-      <div className="w-full h-screen absolute bg-black -ml-7 sm:-ml-10 -translate-x-full"> 
-          <ul><Link to="/">Home</Link></ul>
-          <ul><Link to="/register">Sign Up</Link></ul>
-          <ul><Link to="/login">Login</Link></ul>
+      <div className="w-full h-screen absolute bg-base -ml-7 sm:-ml-10 flex flex-col items-center justify-center -translate-x-full"> 
+          <ul className="mb-10 text-3xl"><Link to="/">Home</Link></ul>
+          <ul className="mb-10 text-3xl"><Link to="/register">Sign Up</Link></ul>
+          <ul className="mb-10 text-3xl"><Link to="/login">Login</Link></ul>
       </div>
     </div>
   )
