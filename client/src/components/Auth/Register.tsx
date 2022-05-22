9b16a93f01b09430368587dae3e02d05842d6a30
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-05-22 14:58:16 +0800

    Completed registration form plus responsiveness

--- client/src/components/Auth/Register.tsx
+++ client/src/components/Auth/Register.tsx
@@ -1,30 +1,37 @@
 import { Link } from 'react-router-dom';
+import { FaUserAlt, FaLock, FaEnvelope, FaCheck } from "react-icons/fa";
 
 const Register = () => {
   return (
     <div className="h-screen flex flex-row">
-      <div className="w-2/5 flex flex-col justify-center items-center">
-        <h1 className="text-[30px] font-bold">Sign Up</h1>
-        <form className="flex flex-col px-20 justify-center">
-          <label htmlFor='username'>
-            Username:
-          </label>
-          <input type="text" id="username" autoComplete='off' required />
-          <label htmlFor='password'>
-            Password:
-          </label>
-          <input type="password" id="passwrod" required />
-          <label htmlFor='confirm_password'>
-            Confirm Password:
-          </label>
-          <input type="password" id="confirm_passwrod" required />
-          <div>
-            Already have an account? <Link to="/login">Sign In</Link>
+      <div className="w-full lg:w-2/5 flex flex-col justify-center items-center">
+        <h1 className="text-[30px] font-bold mb-8">Sign Up</h1>
+        <form className="flex flex-col px-12 justify-center w-full lg:w-5/6 max-w-[500px]">
+          <div className="flex flex-row items-center justify-center mb-5 border-b-2 border-accent pb-2">
+            <i className="text-md mr-3"><FaEnvelope /></i>
+            <input type="text" id="email" autoComplete='off' placeholder="Email" required className="bg-transparent  w-full focus:outline-0"/>
           </div>
-          <button>Sign Up</button>
+          <div className="flex flex-row items-center justify-center mb-5 border-b-2 border-accent pb-2">
+            <i className="text-md mr-3"><FaUserAlt /></i>
+            <input type="text" id="username" autoComplete='off' placeholder="Username" required className="bg-transparent  w-full focus:outline-0"/>
+          </div>
+          <div className="flex flex-row items-center justify-center mb-5 border-b-2 border-accent pb-2">
+            <i className="text-md mr-3"><FaLock /></i>
+            <input type="password" id="password" placeholder="Password" required className="bg-transparent  w-full focus:outline-0" />
+          </div>
+          <div className="flex flex-row items-center justify-center mb-3 border-b-2 border-accent pb-2">
+            <i className="text-md mr-3"><FaCheck /></i>
+            <input type="confirm-password" id="confirm-password" placeholder="Confirm Password" required className="bg-transparent  w-full focus:outline-0" />
+          </div>
+          <div className='text-xs mb-10'>
+            Already have an account? <Link to="/login" className='text-accent ml-1 underline'>Sign In</Link>
+          </div>
+          <div className="flex justify-center items-center">
+            <button className="text-md bg-accent py-2 w-full rounded-lg">Register</button>
+          </div>
         </form>
       </div>
-      <div className="w-3/5 bg-white text-black">image illustrations</div>
+      <div className="hidden lg:block w-3/5  h-full text-black bg-login-img bg-cover brightness-75"></div>
     </div>
   )
 }
